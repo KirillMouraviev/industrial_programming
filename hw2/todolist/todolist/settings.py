42c78a8658b64869e71beef8f34ddf1dbdce7c89
KirillMouraviev <kirill.mouraviev@yandex.ru>
2018-02-26 03:59:49 +0300

    Пока не работает, потом постараюсь доделать

--- hw2/todolist/todolist/settings.py
+++ hw2/todolist/todolist/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'todolist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'todos/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
